2eba12f1598a445c9dbf7a0d708ab33cea73c215
sejal.yadav <sejal.yadav@esparkbizmail.com>
2023-07-24 18:14:41 +0530

    NoteForm :working

--- react-note-taking-app/src/App.tsx
+++ react-note-taking-app/src/App.tsx
@@ -1,12 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Container } from "react-bootstrap";
+import { NewNote } from './components/NewNote';
 function App() {
-
-
   return (
-    <>
-
-
-    </>
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/new' element={<NewNote/>} />
+        <Route path='*' element={<Navigate to="/" />} />
+        <Route path='/:id' >
+          <Route index element={<h1>show</h1>} />
+          <Route path='edit' element={<h1>edit</h1>} />
+        </Route>
+      </Routes>
+    </Container>
   )
 }
 
---
+++ react-note-taking-app/src/components/NoteForm.tsx
@@ -0,0 +1,42 @@
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CreatableReactSelect from "react-select/creatable";
+
+export function NoteForm() {
+    return <>
+        <h1>Form</h1>
+        <Form>
+            <Stack gap={5}>
+                <Row>
+                    <Col>
+                        <Form.Group controlId="title">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control required />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="tags">
+                            <Form.Label>Tags</Form.Label>
+                            <CreatableReactSelect isMulti />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Form.Group controlId="markdown">
+                    <Form.Label>Body</Form.Label>
+                    <Form.Control required as="textarea" rows={15} cols={10} />
+                </Form.Group>
+                <Stack direction="horizontal" gap={2} className="justify-content-end">
+                    <Button type="submit" variant="primary">
+                        Save
+                    </Button>
+                    <Link to="..">
+                        <Button type="button" variant="outline-secondary">
+                            Cancel
+                        </Button>
+                    </Link>
+                </Stack>
+
+            </Stack>
+        </Form>
+    </>
+}
